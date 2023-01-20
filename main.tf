cd5faa96004db8a2d6af2ebc52b58452b7042b5b
lwbengel <ben.engel@lacework.net>
2023-01-20 15:52:52 -0600

    Main

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    lacework = {
+      source = "lacework/lacework"
+      version = "1.4.0"
+    }
+  }
+}
+
+provider "lacework" {
+  # Configuration options
+}
